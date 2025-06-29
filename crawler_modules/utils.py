5c956d1cceaca9b48b561bf416be1e99a2c270e3
Florian <80332665+fgrfn@users.noreply.github.com>
2025-06-29 20:23:33 +0200

    Delete crawler_modules directory

--- crawler_modules/utils.py
+++
@@ -1,8 +0,0 @@
-# crawler_modules/utils.py
-from croniter import croniter
-from datetime import datetime
-
-def next_run_time(cron_expr: str) -> datetime:
-    now = datetime.now()
-    itr = croniter(cron_expr, now)
-    return itr.get_next(datetime)
